79446bac93585d9506aa2469d53c0fe2283f5ecf
Leandro Amaral <lramaral02m@gmail.com>
2025-01-27 14:31:03 -0300

    Criado controller, DTO, Service

---
+++ CRUDUsuariosDapper/Services/IUsuarioInterface.cs
@@ -0,0 +1,10 @@
+﻿using CRUDUsuariosDapper.Dto;
+using CRUDUsuariosDapper.Models;
+
+namespace CRUDUsuariosDapper.Services
+{
+    public interface IUsuarioInterface
+    {
+        Task<ResponseModel<List<UsuarioListarDto>>> BuscarUsuarios();
+    }
+}
---
+++ CRUDUsuariosDapper/Services/UsuarioService.cs
@@ -0,0 +1,19 @@
+﻿using CRUDUsuariosDapper.Dto;
+using CRUDUsuariosDapper.Models;
+
+namespace CRUDUsuariosDapper.Services
+{
+    public class UsuarioService : IUsuarioInterface
+    {
+        private readonly IConfiguration _configuration;
+        public UsuarioService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public Task<ResponseModel<List<UsuarioListarDto>>> BuscarUsuarios()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
