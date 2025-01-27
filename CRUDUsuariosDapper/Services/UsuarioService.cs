135cb1583137bf920737ab44b8f282ff1892f9aa
Leandro Amaral <lramaral02m@gmail.com>
2025-01-27 15:45:30 -0300

    Conectando bancode dados a aplicação

--- CRUDUsuariosDapper/Services/UsuarioService.cs
+++ CRUDUsuariosDapper/Services/UsuarioService.cs
@@ -1,5 +1,7 @@
 ﻿using CRUDUsuariosDapper.Dto;
 using CRUDUsuariosDapper.Models;
+using Dapper;
+using System.Data.SqlClient;
 
 namespace CRUDUsuariosDapper.Services
 {
@@ -11,9 +13,23 @@ namespace CRUDUsuariosDapper.Services
             _configuration = configuration;
         }
 
-        public Task<ResponseModel<List<UsuarioListarDto>>> BuscarUsuarios()
+        public async Task<ResponseModel<List<UsuarioListarDto>>> BuscarUsuarios()
         {
-            throw new NotImplementedException();
+            ResponseModel<List<UsuarioListarDto>> response = new ResponseModel<List<UsuarioListarDto>>(); 
+
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                var usuariosBanco = await connection.QueryAsync<Usuario>("select * from Usuarios");
+                if (usuariosBanco.Count() == 0)
+                {
+                    response.Mensagem = "Nenhum usuário localizado!";
+                    response.Status = false;
+                    return response;
+                }
+                //response.Dados = usuariosBanco;
+                //response.Mensagem = "Usuários localizados com sucesso!";
+            }
+            return response;
         }
     }
 }
